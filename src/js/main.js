bb55eead0835df1756867e0eceb707c36917aee9
Emma Heikkinen <emhe2504@student.miun.se>
2026-01-31 12:21:56 +0100

    Fortsatt arbete med fetch, som nu börjar ta form

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,31 @@
 "use strict"
 
-document.addEventListener("DOMContentLoaded", fetchData);
+document.addEventListener("DOMContentLoaded", fetchData); //När DOM-laddat, anropa fetchData
 
 async function fetchData() {
     const link = "https://webbutveckling.miun.se/files/ramschema.json";
 
     try {
-        const newData = await fetch(link);
+        const newData = await fetch(link); //När fetchData anropas fetchas denna länk
         const data = await newData.json();
 
         createTable(data);
-    } catch(error){
+    } catch (error) {
+        console.error(error);
 
+        const section = document.getElementById("table-section");
+        section.innerHTML = `
+        <article>
+        <h3>Ett fel verkar ha uppstått</h3>
+        <p>Tyvärr kan inte datan laddas för tillfället..</p>
+        </article>
+        `;
     }
 
 }
 
 function createTable(data) {
-    const table = document.getElementById("table");
-    const tbody =document.getElementById("tbody");
+    const tbody = document.getElementById("tbody");
 
     data.forEach(element => {
         tbody.innerHTML += `
