a592c4d1baaa16783952d0d883454b19889fa1ea
Emma Heikkinen <emhe2504@student.miun.se>
2026-01-31 12:55:59 +0100

    Kommentarer

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,18 @@
 
 document.addEventListener("DOMContentLoaded", fetchData); //När DOM-laddat, anropa fetchData
 
-async function fetchData() {
+async function fetchData() { //async - denna funktion returnerar ett promise i form av ett objekt
     const link = "https://webbutveckling.miun.se/files/ramschema.json";
 
     try {
         const newData = await fetch(link); //När fetchData anropas fetchas denna länk
-        const data = await newData.json();
+        const data = await newData.json(); //data = objekt som ska ovandlas till JavaScript
 
-        createTable(data);
+        createTable(data); //skickas vidare till nästa funktion som anropas
     } catch (error) {
         console.error(error);
 
-        const section = document.getElementById("table-section");
+        const section = document.getElementById("table-section"); //Vad som ska ske via error
         section.innerHTML = `
         <article>
         <h3>Ett fel verkar ha uppstått</h3>
@@ -24,10 +24,10 @@ async function fetchData() {
 
 }
 
-function createTable(data) {
+function createTable(data) { //Hit kommer data-objektet
     const tbody = document.getElementById("tbody");
 
-    data.forEach(element => {
+    data.forEach(element => { //För varje "element" i objektet ska nedstående ske (i tabell)
         tbody.innerHTML += `
         <tr>
         <td>${element.code}</td>
